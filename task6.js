ddf62413d61a68f4b31a54cc9766651908fd5fb0
Andrey <56323140+AndreyNazarov@users.noreply.github.com>
2020-07-25 17:31:55 +0200

    with prettier

--- task6.js
+++ task6.js
@@ -1,20 +1,16 @@
 
- let input = prompt("tap a number");
- let total = 0;
+let total = 0;
+const numbers = [];
 do {
-  
-  const numbers = [];
+  let input = prompt("tap a number");
   if (input === null) {
     break;
   }
   input = parseInt(input);
   numbers.push(input);
-  console.log(input);
-
- 
-  
 } while (true);
+
 for (const number of numbers) {
   total += number;
 }
-console.log(total);
+console.log(`Summary: ${total}`);
